1f58caf8f67d1da209a52603246e1bcd46ad4479
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-15 15:40:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(i) {
+  return function(fare) {
+    return i * fare
+  }
+}
+
+const fareDoubler = function(fare) {
+  return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare) {
+  return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers, func) {
+  return func(drivers)
+}
